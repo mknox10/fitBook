4fe00517e31e6768b194b1c278973e556ac19375
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2020-05-07 16:53:54 -0500

    Add validation to calendar page

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import { WorkoutService } from '../workout.service';
-import { Workout } from 'src/workout';
-import { Record } from 'src/record';
-import { FormGroup, FormControl } from '@angular/forms';
+import {WorkoutService} from '../workout.service';
+import {Workout} from 'src/workout';
+import {Record} from 'src/record';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-calendar',
@@ -12,7 +12,8 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class CalendarComponent implements OnInit {
 
-  constructor(private workoutService: WorkoutService) { }
+  constructor(private workoutService: WorkoutService) {
+  }
 
   ngOnInit(): void {
     this.getWorkouts();
@@ -54,60 +55,68 @@ export class CalendarComponent implements OnInit {
     this.calendarWorkouts.push({title: title, date: date, allDay: true, stick: true});
   }
 
+  refreshWorkouts() {
+    this.getWorkouts();
+    this.calendarWorkouts = this.workoutsList.map(workout => ({title: workout.name, date: workout.date, allDay: true, stick: true}));
+  }
+
   /** form used to add a new workout */
   addWorkoutForm = new FormGroup({
-    title: new FormControl(''),
-    date: new FormControl(''),
+    title: new FormControl('', [Validators.required]),
+    date: new FormControl('', [Validators.required]),
     workout: new FormControl('')
-  })
+  });
 
   correctWorkoutDate: string;
+
   addWorkout(event: Event) {
     event.preventDefault();
     this.records = [];
     this.correctWorkoutDate = this.getNewDay(this.addWorkoutForm.get('date').value);
-    this.workoutService.createWorkout(this.addWorkoutForm.get('title').value, new Date(this.correctWorkoutDate), this.records)
-    this.addWorkoutsToCalendar(this.addWorkoutForm.get('title').value, new Date(this.correctWorkoutDate));
+    this.workoutService.createWorkout(this.addWorkoutForm.get('title').value, new Date(this.correctWorkoutDate), this.records);
     this.addWorkoutForm.reset();
+    this.refreshWorkouts();
   }
 
   /** form used to add a past workout */
   addPastWorkoutForm = new FormGroup({
-    title: new FormControl(''),
-    date: new FormControl(''),
-    workout: new FormControl('')
-  })
+    title: new FormControl('', [Validators.required]),
+    date: new FormControl('', [Validators.required]),
+    workout: new FormControl('', [Validators.required])
+  });
 
   /** method to correct for a date bug in the fullcalendar */
   correctDate: string;
   day: string;
   newDay: number;
+
   getNewDay(date: string): string {
     this.correctDate = date.slice(0, 8);
     this.day = date.slice(8, 10);
     this.newDay = +this.day + 1;
-    if(this.newDay < 10) {
+    if (this.newDay < 10) {
       return this.correctDate + '0' + this.newDay;
-    }
-    else {
+    } else {
       return this.correctDate + this.newDay;
     }
   }
 
   /** method creates a new workout using the records from the past workout and adds the new workout to the calendar */
   correctPastWorkoutDate: string;
+
   addPastWorkout() {
     this.correctPastWorkoutDate = this.getNewDay(this.addPastWorkoutForm.get('date').value);
     this.workoutService.createWorkout(this.addPastWorkoutForm.get('title').value, new Date(this.correctPastWorkoutDate), this.addPastWorkoutForm.get('workout').value.records);
-    this.addWorkoutsToCalendar(this.addPastWorkoutForm.get('title').value, new Date(this.correctPastWorkoutDate));
     this.addPastWorkoutForm.reset();
+    this.refreshWorkouts();
   }
 
   /** removes a workout from the calendar but does not delete the workout from the WORKOUTS database */
   index: number;
-  deleteWorkout(workout: any) {
-    this.index = this.calendarWorkouts.indexOf(workout);
-    this.calendarWorkouts.splice(this.index, 1);
+
+  deleteWorkout(workout: Workout) {
+    this.workoutService.removeWorkout(workout.id);
+    this.refreshWorkouts();
   }
 
 
